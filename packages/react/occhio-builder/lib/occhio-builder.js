7678dfa25f6f0d289d4d7d9de27205ce19c26bb3
Vinamra Sareen <vsareen24@gmail.com>
2021-06-07 22:34:32 +0530

    reverted changes for prop-types

--- packages/react/occhio-builder/lib/occhio-builder.js
+++ packages/react/occhio-builder/lib/occhio-builder.js
@@ -5,7 +5,6 @@ const resolve = require("@rollup/plugin-node-resolve").default;
 const babel = require("@rollup/plugin-babel").default;
 const postcss = require("rollup-plugin-postcss");
 const { uglify } = require("rollup-plugin-uglify");
-const commonjs = require("rollup-plugin-commonjs");
 
 const currentWorkingPath = process.cwd();
 const { src, name } = require(path.join(currentWorkingPath, "package.json"));
@@ -26,28 +25,10 @@ const inputOptions = {
       modules: true,
     }),
     uglify(),
-    commonjs({
-      namedExports: {
-        "../../node_modules/prop-types/index.js": ["isValidElementType"],
-      },
-      include: [
-        /node_modules\/prop-types/,
-        /node_modules\/hoist-non-react-statics/,
-        /node_modules\/invariant/,
-        /node_modules\/react-is/,
-        /node_modules\/warning/,
-      ],
-    }),
     babel({
       presets: ["@babel/preset-env", "@babel/preset-react"],
       babelHelpers: "bundled",
       exclude: "node_modules/**",
-      overrides: [
-        {
-          test: "./node_modules",
-          sourceType: "unambiguous",
-        },
-      ],
     }),
   ],
 };
